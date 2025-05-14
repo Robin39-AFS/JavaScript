44fcaf9eaba0f2b92565789ad19e1d04f7c77068
Robin <afsarulamin10@gmail.com>
2025-05-14 17:06:32 +0600

    Some practice and understand the hoisting

---
+++ repo.js
@@ -0,0 +1,37 @@
+
+// getName();
+
+// console.log(x);
+// console.log(getName);
+
+// var x = 7;
+
+// function getName(){
+//     console.log("Hello JavaScript");
+// }
+
+
+// var x = 1;
+
+// a();
+// b();
+
+// console.log(x);
+
+// function a() {
+//     var x =10;
+//     console.log(x);
+// }
+// function b() {
+//     var x =100;
+//     console.log(x);
+// }
+
+var a = 10;
+
+function b(){
+    var x = 20;
+}
+
+// console.log(b().x);
+console.log(window.a);
