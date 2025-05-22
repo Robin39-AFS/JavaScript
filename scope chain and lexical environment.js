7c6ed1d3e09aa62474b917f023cedbdc30ceb8de
Robin <afsarulamin10@gmail.com>
2025-05-22 20:17:06 +0600

    Lexical environment and scope chain

--- scope chain and lexical environment.js
+++ scope chain and lexical environment.js
@@ -24,7 +24,22 @@ function a() {
 a();
 
 console.log(b);
+// We have to remember that when a function invoked new execution context create
 
 // There also print b even we b declare b inside the function a but for this we can't access b in global scope but invoked a we can access b's value
 
 // But if we declare b in a function and try to print then it will tell it's not defined in and it's actually scope
+
+// Scope is directly dependent on the lexical environment
+
+// Wherever an execution context is created a lexical environment is also created , 
+// So, lexical environment is the local memory along with the lexical environment of it's parent 
+
+// What is lexical it's a term means in hierarchy or in a sequence in code concept we can tell c() is lexically sitting inside a() function.
+
+// This orange thing is pointing to the lexical environment of it's parent 
+
+// Fun thing is the global execution has lexical parent reference to the null
+
+// Searching in the lexical reference is called scope chain
+
